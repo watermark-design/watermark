57f9c6282f0733f0d27dc1681d79ccc07156dd16
MichaelSun <zhensherlock@126.com>
2023-11-05 13:20:19 +0800

    docs: update playground-node

--- playground-node/node/main.ts
+++ playground-node/node/main.ts
@@ -1,11 +1,11 @@
 import * as watermark from '../../packages/node/src/main';
 
 watermark.blind
-  .decodeBlindWatermark({
+  .decode({
     url: 'https://cdn.jsdelivr.net/gh/zhensherlock/oss@main/uPic/blind-watermark-test-bxEJgQ.png',
-    fillColor: '#000',
-    compositeOperation: 'color-burn',
-    mode: 'canvas',
+    // fillColor: '#000',
+    // compositeOperation: 'color-burn',
+    // mode: 'canvas',
   })
   .then((res) => {
     console.log(res);
