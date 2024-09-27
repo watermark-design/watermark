f342c5e22117ee4825d47d6710bdce6bdf41c05f
MichaelSun <zhensherlock@126.com>
2024-09-27 13:23:25 +0800

    docs: add algolia search

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,4 +1,5 @@
 import DefaultTheme from 'vitepress/theme';
+import ElementPlus from 'element-plus';
 import './custom-theme.scss';
 import 'element-plus/dist/index.css';
 import 'element-plus/theme-chalk/dark/css-vars.css';
@@ -7,9 +8,7 @@ export default {
   enhanceApp: async ({ app, router, siteData, isServer }: any) => {
     // app is the Vue 3 app instance from `createApp()`. router is VitePress'
     // custom router. `siteData`` is a `ref`` of current site-level metadata.
-    import('element-plus').then((module) => {
-      app.use(module);
-    });
+    app.use(ElementPlus);
     // import('@watermark-design/vue').then((module) => {
     //   app.use(module.default);
     // });
